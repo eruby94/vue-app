b216acfb8167343adba637a3a7396d76b294d1cd
Evan Ruby <evanruby94@gmail.com>
2017-09-29 14:18:01 -0400

    Add graphcool to project, prepare for GrapqhQL integration via apollo

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import VueApollo from 'vue-apollo'
+import { ApolloClient, createBatchingNetworkInterface } from 'apollo-client'
 import VueLocalStorage from 'vue-localstorage'
 import VModal from 'vue-js-modal'
 
@@ -11,9 +13,28 @@ Vue.config.productionTip = false
 Vue.use(VueLocalStorage)
 Vue.use(VModal)
 
+const networkInterface = createBatchingNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj85z9wpe00p40124qil35fsd'
+})
+
+const apolloClient = new ApolloClient({
+  networkInterface,
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  apolloProvider,
   router,
   store,
   template: '<App/>',
